6349ce5d70e0596016066536ecf20ffbf6ba7188
Chuckie <devchuckcamp@gmail.com>
2023-02-20 16:15:28 -0800

    Sender Service

---
+++ AzureServiceBusSender/Repository/QueueService.cs
@@ -0,0 +1,25 @@
+﻿using AzureServiceBusSender.Services;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace AzureServiceBusSender.Repository;
+
+public class QueueService:IQueueService
+{   
+    private readonly IConfiguration _config;
+    public QueueService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SendMessageAsync<T>(T serviceBusMessage, string queueName)
+    {
+        var queueClient = new QueueClient(_config.GetConnectionString("AzureServiceBus"), queueName);
+        string messageBody = JsonSerializer.Serialize(serviceBusMessage);
+        var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+        await queueClient.SendAsync(message);
+    }
+}
---
+++ AzureServiceBusSender/Services/IQueueService.cs
@@ -0,0 +1,6 @@
+﻿namespace AzureServiceBusSender.Services;
+
+public interface IQueueService
+{
+    Task SendMessageAsync<T>(T message, string queueName);
+}
