b05d6c6a6de02bd8381c3d4f0bf79f9c89382805
Chuckie <devchuckcamp@gmail.com>
2023-02-20 16:16:21 -0800

    Common Resources

---
+++ Common/Models/PickItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models;
+
+public class PickItem
+{
+    public int OrderId { get; set; }
+    public string? ItemSku { get; set; }
+}
+
